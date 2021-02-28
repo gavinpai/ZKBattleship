fd24a1670a786db9140c3ce5febbb23672eb9d66
SteelShredder <gavingavington@gmail.com>
2021-02-28 01:27:13 -0800

    Added generation time bar graph

--- stats.py
+++ stats.py
@@ -1,5 +1,7 @@
 import pedersen
 from matplotlib import pyplot as plt
+import time
+import statistics
 def pedersen_histogram():
     x= 1000000
     gen = pedersen.Pedersen(256)
@@ -21,4 +23,28 @@ def pedersen_histogram():
     plt.ylabel("Occurences")
     plt.xlabel("Commitment value")
     plt.show()
-pedersen_histogram()
+
+def time_generate():
+    c = []
+    x = 1000
+    a = ("16", "32", "64", "128", "256")
+    times = [[],[],[],[],[]]
+    t = []
+    e = []
+    for i in range(len(a)):
+        for j in range(x):
+            start = time.time()
+            gen = pedersen.Pedersen(2 ** (4 + i))
+            end = time.time()
+            times[i].append(end - start)
+        t.append(statistics.fmean(times[i]))
+        e.append(statistics.stdev(times[i]))
+    
+    plt.ylabel("Bits")
+    plt.xlabel("Time (s)")
+    plt.title("Commitment Bit Length vs Time of Generation")
+    plt.barh(a, t, xerr = e)
+    plt.show()
+        
+
+time_generate()
